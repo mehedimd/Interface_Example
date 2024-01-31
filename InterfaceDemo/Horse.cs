0c09f71d754a5039f74f89db13edf97d46b935f8
WADA_58 <WADA_58@DESKTOP-5HQIB3T>
2024-01-31 15:01:29 +0600

    Add project files.

---
+++ InterfaceDemo/Horse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    internal class Horse: ILandBound, IJourney
+    {
+        int ILandBound.NumberOfLegs()
+        {
+            return 500;
+        }
+         int IJourney.NumberOfLegs()
+        {
+            return 600;
+        }
+        static void Main(string[] args)
+        {
+            Horse horse = new Horse();
+            ILandBound ilb = horse;
+            int x = ilb.NumberOfLegs();
+            Console.WriteLine(x);
+
+            IJourney journey = horse;
+            int y = journey.NumberOfLegs();
+            Console.WriteLine(y);  
+
+            Console.ReadKey();
+            
+        }
+    }
+}
